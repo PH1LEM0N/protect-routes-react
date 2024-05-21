a0d51732c84107960b625456c835ef3c7a7658c0
BlaueTulpe <phi.weber@proton.me>
2024-05-21 23:14:49 +0200

    .

--- src/user-details-provider.js
+++ src/user-details-provider.js
@@ -7,11 +7,11 @@ export const STORAGE_USER_ITEM_KEY = "authenticated";
 export const userDetailsContext = createContext();
 
 const UserDetailsProvider = (props) => {
-        // this state will be shared with all components 
+    // this state will be shared with all components 
     const [userDetails, setUserDetails] = useState(localStorage.getItem(STORAGE_USER_ITEM_KEY) || false);
 
     return (
-                // this is the provider providing state
+        // this is the provider providing state
         <userDetailsContext.Provider value={[userDetails, setUserDetails]}>
             {props.children}
         </userDetailsContext.Provider>
